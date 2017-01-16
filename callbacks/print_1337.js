b37c9e27c5bfaf114cd98ccf633a4de3ccd2a84d
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-16 19:07:19 +0000

    Implemented 1337-translating function.

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -8,8 +8,17 @@ var requestOptions = {
 function print1337 (html) {
 
   /* Write your code here! */
-  console.log(html.toUpperCase());
+  var oldBit = ['ck', 'er', 'you', 'a', 'e', 'o', 'l', 's', 't'];
+  var newBit = ['x', '0r', 'j00', '4', '3', '0', '1', '5', '7'];
 
+  var html1337 = html;
+
+  for (var i = 0; i < oldBit.length; i++) {
+    var regex = new RegExp(oldBit[i], 'g');
+    html1337 = html1337.replace(regex, newBit[i]);
+  }
+
+  console.log(html1337);
 }
 
 getHTML(requestOptions, print1337);
